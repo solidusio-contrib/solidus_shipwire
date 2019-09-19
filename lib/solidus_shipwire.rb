0f2edf17534be48f16b51863b9177ebb33c4e6b9
Alessandro Desantis <desa.alessandro@gmail.com>
2019-09-19 18:22:35 +0200

    Remove lib from the autoload paths

--- lib/solidus_shipwire.rb
+++ lib/solidus_shipwire.rb
@@ -7,9 +7,10 @@ require 'shipwire'
 require 'active_model_serializers'
 
 require 'solidus_shipwire/engine'
-require 'solidus_shipwire/response_decorator'
 require 'solidus_shipwire/response_exception'
 require 'solidus_shipwire/shipwireable'
+require 'solidus_shipwire/shipwireable/shipwire_api'
+require 'solidus_shipwire/shipwireable/shipwire_serializer'
 
 ActiveSupport.on_load(:active_record) do
   extend SolidusShipwire::Shipwireable
