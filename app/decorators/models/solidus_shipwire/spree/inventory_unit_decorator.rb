813c621679aff3f9edfebc61fd8aa6664f81b664
Alessandro Desantis <desa.alessandro@gmail.com>
2019-09-19 18:22:05 +0200

    Remove useless base scope reference from InventoryUnitDecorator

--- app/decorators/models/solidus_shipwire/spree/inventory_unit_decorator.rb
+++ app/decorators/models/solidus_shipwire/spree/inventory_unit_decorator.rb
@@ -1,7 +1,7 @@
 module SolidusShipwire
   module InventoryUnitDecorator
     def self.prepended(base)
-      base.scope :eligible_for_shipwire, -> { base.joins(:variant).where.not(spree_variants: { shipwire_id: nil }) }
+      base.scope :eligible_for_shipwire, -> { joins(:variant).where.not(spree_variants: { shipwire_id: nil }) }
     end
 
     Spree::InventoryUnit.prepend self
